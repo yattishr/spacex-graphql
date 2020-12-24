f0eb46c1dd4c4cace2ac387ae10418cf71ea909b
yattishr <yattish@gmail.com>
2020-12-24 14:30:25 +0200

    Final stage, push to Herkou.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { graphqlHTTP } = require('express-graphql');
 const app = express()
 const cors = require('cors');
 const schema = require('./schema.js')
-
+const path = require('path');
 
 // Allow cross-origin
 app.use(cors());
@@ -12,7 +12,14 @@ app.use(cors());
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     graphiql: true
-}))
+}));
+
+app.use(express.static('public'));
+
+// redirct the user back to the React front-end when they hit Root dir
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`GraphQL Server Runninng on port...${PORT}`));
+app.listen(PORT, () => console.log(`GraphQL Server Runninng on port: ${PORT}`));
